17d0664548defde73ae99d33fee1b5f2160b7b47
maciej022 <maciejpieper56@gmail.com>
2024-04-15 13:37:31 +0200

    polaczenie2plikow

--- Powtorzeniowy/Program.cs
+++ Powtorzeniowy/Program.cs
@@ -1,7 +1,13 @@
-﻿class PlikGłówny
+﻿using Powtorzeniowy;
+
+class PlikGłówny
 {
+
     static void Main(string[] args)
     {
-
+        //To jest baza naszego projektu
+        Tablice tablice = new Tablice();
+        tablice.Wyswietl();
     }
+        
 }
