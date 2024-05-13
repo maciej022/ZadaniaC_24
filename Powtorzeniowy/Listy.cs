e63fbc04435517c10e58b78b55adb972abff0982
CHoina <ess.nikodem.laga@gmail.com>
2024-05-13 14:09:11 +0200

    Dodawanie

---
+++ Powtorzeniowy/Listy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powtorzeniowy
+{
+    public class Listy
+    {
+
+
+        public List<int> Main()
+        {
+            List<int> Lista = new List<int>();
+            string decyzja = "";
+            while (decyzja == "t")
+            {
+                Console.WriteLine("Czy chcesz dodać element do listy?");
+                decyzja = Console.ReadLine();
+                if (decyzja == "t")
+                {
+                    Console.WriteLine("Podaj element: ");
+                    int element = int.Parse(Console.ReadLine());
+                    Lista.Add(element);
+
+                }
+                else
+                {
+                    Console.WriteLine("Podano wszystkie elementy");
+                }
+            }
+            return Lista;
+        }
+    }
+}
+
+        
+        
+
+            
+
--- Powtorzeniowy/Program.cs
+++ Powtorzeniowy/Program.cs
@@ -5,17 +5,15 @@ class PlikGłówny
 
     static void Main(string[] args)
     {
-        //To jest baza naszego projektu
+        Listy abc = new Listy();
+        abc.Dodawanie();
+        Console.WriteLine(abc.Dodawanie1(123, 5));
+
         Tablice tablice = new Tablice();
         
       int[] tab =  tablice.Towrzenie_tablicy();
         tablice.Wyswietl(tab);
-
         tablice.SortowanieBabelkowe(tab);
-
-        
-       
-
         tablice.Przeszukaj(tab);
         
     }
